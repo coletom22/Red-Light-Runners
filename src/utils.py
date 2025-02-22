214331645ce565923f1cb7bbc1a010d7dede9588
Cole Tom <coletom22@gmail.com>
2025-02-21 23:49:23 -0800

    added viewing_to_yolo helper function that transforms annotations into yolo format and returns them as a list

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,16 @@
 # add function for converting rectangle points and class value to YOLO format
+def viewing_to_yolo(annotations, IMAGE_WIDTH, IMAGE_HEIGHT) -> list[dict]:
+    yolo_annotations = []
+
+    for ann in annotations:
+        x1, x2, y1, y2, class_id = ann['x1'], ann['x2'], ann['y1'], ann['y2'], ann['class_id']
+
+        # compute yolo format values
+        x_center = (x1 + x2) / 2 / IMAGE_WIDTH
+        y_center = (y1 + y2) / 2 / IMAGE_HEIGHT
+        width = (x2 - x1) / IMAGE_WIDTH
+        height = (y2 - y1) / IMAGE_HEIGHT
+
+        yolo_annotations.append((class_id, round(x_center, 6), round(y_center,6), round(width, 6), round(height, 6)))
+
+    return yolo_annotations
